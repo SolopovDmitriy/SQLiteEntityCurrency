137df9a6bb0d19c7939b924b8b5fb3d2c8563a56
Dmitriy Solopov <div.solopov@gmail.com>
2021-05-15 14:53:52 +0300

    first commit

---
+++ WpfApp1/CurrencyWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для CurrencyWindow.xaml
+    /// </summary>
+    public partial class CurrencyWindow : Window
+    {
+        CurrencyConverter currencyConverter;
+
+
+        public CurrencyWindow()
+        {
+            InitializeComponent();           
+           
+            currencyConverter = new CurrencyConverter();
+            List<string> currencyList  = new List<string>(currencyConverter.Currencies.Keys);
+            currencyList.Sort();
+            ComboBox_CurrencyTo.ItemsSource = currencyList;
+            ComboBox_CurrencyFrom.ItemsSource = currencyList;            
+        }
+
+       
+
+        private void Button_ConvertCurrency_Click(object sender, RoutedEventArgs e)
+        {
+            
+            double moneyFrom;
+            bool isSuccessfull = Double.TryParse(TextBox_CurrencyFrom.Text, out moneyFrom);
+            if (!isSuccessfull)
+            {
+                MessageBox.Show("money is not a number");
+                return;
+            }
+            string currencyFrom = ComboBox_CurrencyFrom.SelectedItem as string;
+            string currencyTo = ComboBox_CurrencyTo.SelectedItem as string;
+            double moneyTo = moneyFrom * currencyConverter.Currencies[currencyFrom].Rate /
+                currencyConverter.Currencies[currencyTo].Rate;
+            TextBox_CurrencyTo.Text = moneyTo.ToString();
+            
+        }
+    }
+}
---
+++ WpfApp1/Models/Currency.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.Models
+{
+    class Currency
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public double Rate { get; }
+
+        public string ShortName { get; }
+
+        // public DateTime ExchangeDate { get; }
+
+        public Currency(int id, string name, double rate, string shortName)
+        {
+            Id = id;
+            Name = name;
+            Rate = rate;
+            ShortName = shortName;
+            // ExchangeDate = exchangeDate;
+        }
+
+        public override string ToString()
+        {
+            return $"id: {Id}; name: {Name}; rate: {Rate}; short name: {ShortName} ";
+        }
+    }
+}
